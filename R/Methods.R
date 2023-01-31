ca1df924061a381d122e30dc58cf426ffbc5fe0a
YPP <ypp@stat.ubc.ca>
2023-01-30 17:32:52 -0800

    update full batch PMF; additional utilities

--- R/Methods.R
+++ R/Methods.R
@@ -19,24 +19,24 @@
 #' @param .sample.col.row Take column-wise MH step (default: TRUE)
 #'
 #'
-fit.asap <- function(mtx.file,
-                     k,
-                     pb.factors = k,
-                     num.proj = 3,
-                     em.step = 100,
-                     e.step = 1,
-                     deg.step = 0,
-                     .burnin = 10,
-                     .thining = 3,
-                     a0 = 1,
-                     b0 = 1,
-                     index.file = paste0(mtx.file, ".index"),
-                     verbose = TRUE,
-                     num.threads = 1,
-                     block.size = 100,
-                     eval.llik = FALSE,
-                     .rand.seed = 42,
-                     .sample.col.row = TRUE){
+fit.topic.asap <- function(mtx.file,
+                           k,
+                           pb.factors = k,
+                           num.proj = 3,
+                           em.step = 100,
+                           e.step = 1,
+                           deg.step = 0,
+                           .burnin = 10,
+                           .thining = 3,
+                           a0 = 1,
+                           b0 = 1,
+                           index.file = paste0(mtx.file, ".index"),
+                           verbose = TRUE,
+                           num.threads = 1,
+                           block.size = 100,
+                           eval.llik = FALSE,
+                           .rand.seed = 42,
+                           .sample.col.row = TRUE){
 
     if(!file.exists(index.file)){
         mmutil_build_index(mtx.file, index.file)
@@ -80,6 +80,11 @@ fit.asap <- function(mtx.file,
                          rseed = .rand.seed,
                          NUM_THREADS = num.threads)
 
+    .multinom <- pmf2topic(.nmf$row$mean, .nmf$column$mean)
+    .nmf$beta <- .multinom$beta
+    .nmf$prop <- .multinom$prop
+    .nmf$depth <- .multinom$depth
+
     message("Phase III: The loading parameters for the original column vectors")
 
     beta <- .nmf$row$mean
@@ -101,17 +106,107 @@ fit.asap <- function(mtx.file,
 
     message("normalizing the estimated model parameters")
 
-    ## The same idea borrowed from `fastTopics`
-    uu <- apply(beta, 2, sum)
-    beta <- sweep(beta, 2, uu, `/`)
-    prop <- sweep(asap$theta, 2, uu, `*`)
-    zz <- apply(t(prop), 2, sum)
-    prop <- sweep(prop, 1, zz, `/`)
+    .multinom <- pmf2topic(beta, asap$theta)
 
-    asap$beta <- beta
-    asap$prop <- prop
-    asap$depth <- zz
+    asap$beta <- .multinom$beta
+    asap$prop <- .multinom$prop
+    asap$depth <- .multinom$depth
     asap$nmf <- .nmf
 
     return(asap)
+}
+
+
+#' Fit a topic model by full column-wise iterations
+#'
+#' @param mtx.file data matrix file in a matrix market format
+#' @param k number of topics
+#' @param em.step Monte Carlo EM steps (default: 100)
+#' @param e.step Num of E-step MCMC steps (default: 1)
+#' @param deg.step Num of Degree calibration steps (default: 0)
+#' @param .burnin burn-in period in the record keeping (default: 10)
+#' @param .thining thining for the record keeping (default: 3)
+#' @param a0 Gamma(a0, b0) prior (default: 1)
+#' @param b0 Gamma(a0, b0) prior (default: 1)
+#' @param index.file a file for column indexes (default: "{mtx.file}.index")
+#' @param verbose verbosity
+#' @param num.threads number of threads (default: 1)
+#' @param eval.llik evaluate log-likelihood trace in PMF (default: FALSE)
+#' @param .rand.seed random seed (default: 42)
+#' @param .sample.col.row Take column-wise MH step (default: TRUE)
+#'
+#'
+fit.topic.full <- function(mtx.file,
+                           k,
+                           pb.factors = k,
+                           num.proj = 3,
+                           em.step = 100,
+                           e.step = 1,
+                           deg.step = 0,
+                           .burnin = 10,
+                           .thining = 3,
+                           a0 = 1,
+                           b0 = 1,
+                           index.file = paste0(mtx.file, ".index"),
+                           verbose = TRUE,
+                           num.threads = 1,
+                           eval.llik = FALSE,
+                           .rand.seed = 42,
+                           .sample.col.row = TRUE){
+
+    message("Reading in the full data ...")
+
+    Y <- read.mtx.dense(mtx.file = mtx.file,
+                        memory.idx.file = index.file,
+                        verbose = verbose,
+                        num.threads = num.threads)
+
+    message("Start estimating a PMF model in the full data ...")
+
+    ret <- asap_fit_nmf(Y,
+                        maxK = k,
+                        mcem = em.step,
+                        burnin = .burnin,
+                        do_sample_col_row = .sample.col.row,
+                        latent_iter = e.step,
+                        degree_iter = deg.step,
+                        thining = .thining,
+                        verbose = verbose,
+                        eval_llik = eval.llik,
+                        a0=a0,
+                        b0=b0,
+                        rseed = .rand.seed,
+                        NUM_THREADS = num.threads)
+
+    message("normalizing the estimated model parameters")
+
+    .multinom <- pmf2topic(ret$row$mean, ret$column$mean)
+
+    ret$beta <- .multinom$beta
+    ret$prop <- .multinom$prop
+    ret$depth <- .multinom$depth
+
+    return(ret)
+}
+
+#' Convert Poisson Matrix Factorization to Multinomial Topic model.
+#' The same idea was first coined by `fastTopics` paper.
+#'
+#' @details
+#' E[Y|.beta, .theta] = `.beta %*% t(.theta)`
+#'
+#' @param .beta D x K dictionary matrix
+#' @param .theta N x K sample-wise factor loading matrix
+#'
+#' @return a list of (beta, prop, depth), where the beta is a re-scaled dictionary matrix, each row of the prop matrix corresponds to a mixing proportion per column, and the depth parameter gauges a sequencing depth for each column
+#'
+pmf2topic <- function(.beta, .theta) {
+
+    uu <- apply(.beta, 2, sum)
+    beta <- sweep(.beta, 2, uu, `/`)
+    prop <- sweep(.theta, 2, uu, `*`)
+    zz <- apply(t(prop), 2, sum)
+    prop <- sweep(prop, 1, zz, `/`)
+
+    list(beta = beta, prop = prop, depth = zz)
 }
---
+++ R/Wrapper.R
@@ -0,0 +1,191 @@
+#' A wrapper function to read a sparse submatrix
+#'
+#' @param mtx.file matrix market file
+#' @param sub.cols column index (default: all)
+#' @param memory.idx memory locations
+#' @param memory.idx.file memory location file
+#' @param verbose verbosity
+#'
+#' @return a sparse matrix
+#'
+read.mtx.sparse <- function(mtx.file,
+                            sub.cols = NULL,
+                            memory.idx = NULL,
+                            memory.idx.file = paste0(mtx.file,".index"),
+                            verbose = FALSE,
+                            num.threads = 1) {
+
+    if(is.null(memory.idx)) {
+        if(!file.exists(memory.idx.file))
+            mmutil_build_index(mtx.file, memory.idx.file)
+
+        memory.idx <- mmutil_read_index(memory.idx.file)
+    }
+
+    if(is.null(sub.cols)) {
+        .info <- mmutil_info(mtx.file)
+        sub.cols <- 1:.info$max.col
+    }
+
+    stopifnot(length(sub.cols) == length(unique(sub.cols)))
+
+    .in <-
+        mmutil_read_columns_sparse(mtx.file,
+                                   memory.idx,
+                                   sub.cols,
+                                   verbose,
+                                   NUM_THREADS = num.threads)
+
+    Matrix::sparseMatrix(i=.in$row, j=.in$col, x=.in$val,
+                         dims = c(.in$max.row, .in$max.col))
+}
+
+#' A wrapper function to read a dense submatrix
+#'
+#' @param mtx.file matrix market file
+#' @param sub.cols column index (default: all)
+#' @param memory.idx memory locations
+#' @param memory.idx.file memory location file
+#' @param verbose verbosity
+#'
+#' @return a dense matrix
+#'
+read.mtx.dense <- function(mtx.file,
+                           sub.cols = NULL,
+                           memory.idx = NULL,
+                           memory.idx.file = paste0(mtx.file,".index"),
+                           verbose = FALSE,
+                           num.threads = 1) {
+
+    if(is.null(memory.idx)) {
+        if(!file.exists(memory.idx.file))
+            mmutil_build_index(mtx.file, memory.idx.file)
+
+        memory.idx <- mmutil_read_index(memory.idx.file)
+    }
+
+    if(is.null(sub.cols)) {
+        .info <- mmutil_info(mtx.file)
+        sub.cols <- 1:.info$max.col
+    }
+
+    stopifnot(length(sub.cols) == length(unique(sub.cols)))
+
+    mmutil_read_columns(mtx.file,
+                        memory.idx,
+                        sub.cols,
+                        verbose,
+                        NUM_THREADS = num.threads)
+}
+
+#' Read a vector of string names
+#' @param .file file name
+#' @return a vector of strings
+read.vec <- function(.file) {
+
+    .ends.with <- function(x, pat) {
+        .len <- nchar(x)
+        .end <- base::substr(x, .len - nchar(pat) + 1, .len)
+        return(.end == pat)
+    }
+
+    if(.ends.with(.file, ".gz")) {
+        con <- gzfile(.file)
+        ret <- readLines(con)
+        close(con)
+    } else {
+        ret <- readLines(.file)
+    }
+    return(ret)
+}
+
+#' Create a list of MTX-related files
+#' @param .hdr file set header name
+#' @return a list of file names
+fileset.list <- function(.hdr){
+    .names <- c("mtx","row","col","idx")
+    .out <- paste0(.hdr, c(".mtx.gz", ".rows.gz", ".cols.gz", ".mtx.gz.index"))
+    .out <- as.list(.out)
+    names(.out) <- .names
+    .out
+}
+
+#' Write matrix market file set
+#'
+#' @param out.mtx a sparse matrix
+#' @param out.rows a vector of rows
+#' @param out.cols a vector of columns
+#' @param output output file set header
+#'
+write.sparse <- function(out.mtx, out.rows, out.cols, output){
+
+    out.mtx.file <- paste0(output, ".mtx.gz")
+    out.cols.file <- paste0(output, ".cols.gz")
+    out.rows.file <- paste0(output, ".rows.gz")
+
+    .write.vec <- function(.vec, .file) {
+        con <- gzfile(.file)
+        cat(.vec, file=con, sep="\n")
+        close(con)
+    }
+
+    mmutil_write_mtx(out.mtx, out.mtx.file)
+    .write.vec(out.cols, out.cols.file)
+    .write.vec(out.rows, out.rows.file)
+
+    return(fileset.list(output))
+}
+
+#' A wrapper function that concatenates two files vertically
+#'
+#' @param top.hdr a file header for the top files
+#' @param bottom.hdr a file header for the bottom files
+#' @param out.hdr output file header
+#'
+#' @return a list of the resulting file names
+vcat.sparse <- function(top.hdr, bottom.hdr, out.hdr){
+
+    dir.create(dirname(out.hdr), recursive = TRUE, showWarnings = FALSE)
+
+    out.mtx.file <- paste0(out.hdr, ".mtx.gz")
+    out.cols.file <- paste0(out.hdr, ".cols.gz")
+    out.rows.file <- paste0(out.hdr, ".rows.gz")
+
+    top.mtx.file <- paste0(top.hdr, ".mtx.gz")
+    top.rows.file <- paste0(top.hdr, ".rows.gz")
+    top.cols.file <- paste0(top.hdr, ".cols.gz")
+    bottom.mtx.file <- paste0(bottom.hdr, ".mtx.gz")
+    bottom.rows.file <- paste0(bottom.hdr, ".rows.gz")
+    bottom.cols.file <- paste0(bottom.hdr, ".cols.gz")
+
+    .files <- c(top.mtx.file,
+                top.rows.file,
+                top.cols.file,
+                bottom.mtx.file,
+                bottom.rows.file,
+                bottom.cols.file)
+
+    stopifnot(all(file.exists(.files)))
+
+    top.mtx <- read.mtx.sparse(top.mtx.file)
+    bottom.mtx <- read.mtx.sparse(bottom.mtx.file)
+
+    top.cols <- read.vec(top.cols.file)
+    top.rows <- read.vec(top.rows.file)
+
+    bottom.cols <- read.vec(bottom.cols.file)
+    bottom.rows <- read.vec(bottom.rows.file)
+
+    .top.idx <- which(top.cols %in% bottom.cols)
+    .bottom.idx <- match(top.cols[.top.idx], bottom.cols)
+
+    ## no overlapping row names
+    stopifnot(!any(top.rows %in% bottom.rows))
+
+    out.mtx <- rbind(top.mtx[, .top.idx, drop = FALSE],
+                     bottom.mtx[, .bottom.idx, drop = FALSE])
+
+    out.rows <- c(top.rows, bottom.rows)
+    out.cols <- top.cols[.top.idx]
+    write.sparse(out.mtx, out.rows, out.cols, out.hdr)
+}
